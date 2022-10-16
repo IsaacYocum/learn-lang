f595b4ad5940e8e015541233c7978d25739247ef
isaac.yocum <isyocum@gmail.com>
2022-10-16 16:49:00 -0700

    fixed word tooltip persisting when not hovering

--- src/components/Word.js
+++ src/components/Word.js
@@ -3,9 +3,9 @@ import ViewTextEditorContext from '../contexts/ViewTextEditorContext'
 import './Word.css'
 import WordDetails from './WordDetails'
 
-const Word = ({ wordObj, sentence, expressionsList }) => {
+const Word = ({ word, wordObj, sentence, expressionsList }) => {
     const [isHovering, setIsHovering] = useState(false)
-    const {setWordToEdit} = useContext(ViewTextEditorContext)
+    const { setWordToEdit } = useContext(ViewTextEditorContext)
 
     const handleWordClick = () => {
         console.log('Clicked on: ', wordObj)
@@ -13,25 +13,17 @@ const Word = ({ wordObj, sentence, expressionsList }) => {
         setWordToEdit(wordObj)
     }
 
-    if (isHovering) {
-        return (
-            <span className='tooltip'>
-                <span className={`word hover-highlight familiarity-highlight-${wordObj.familiarity}`}
-                    onClick={handleWordClick}>
-                    {wordObj.word}
-                </span>
-                <WordDetails word={wordObj} sentence={sentence} expressionsList={expressionsList} />
-            </span>
-        )
-    }
-
     return (
         <span className='tooltip'>
-            <span className={`word hover-highlight familiarity-highlight-${wordObj.familiarity}`}
+            <span
                 onMouseEnter={() => setIsHovering(true)}
                 onMouseLeave={() => setIsHovering(false)}
-                onClick={handleWordClick}>
-                {wordObj.word}
+                onClick={handleWordClick}
+            >
+                <span className={`word hover-highlight familiarity-highlight-${wordObj.familiarity}`}>
+                    {word}
+                </span>
+                {isHovering ? <WordDetails word={wordObj} sentence={sentence} expressionsList={expressionsList} /> : null}
             </span>
         </span>
     )
