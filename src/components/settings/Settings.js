ed471d0b2a64c511039743d73e40c5f583642877
isaac <isyocum@gmail.com>
2022-11-20 19:13:56 -0800

    Fix rerender error in Settings.js

--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -3,11 +3,12 @@ import HeaderContext from '../../contexts/HeaderContext'
 
 export const Settings = () => {
     const { setHeaderState } = useContext(HeaderContext);
+
     useEffect(() => {
         setHeaderState({
             title: 'Settings'
         })
-    })
+    }, [setHeaderState])
 
     return (
         <div>Settings</div>
